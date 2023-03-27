d3ad0470ba52d02c228c327a007f68fce7007309
Afudu <abdoulseydou@hotmail.com>
2023-03-27 12:03:00 +0100

    Create carousel_animation.js

---
+++ js/carousel_animation.js
@@ -0,0 +1,61 @@
+// Script adding event listeners to the left and right arrows in the carousels.
+
+// Define variables
+const desktop = window.matchMedia("(min-width: 640px)");
+const positions = [0, 0, 0, 0]
+
+for (let i = 0; i < 4; i++) {
+
+    let right_arrow = document.getElementsByClassName("right-arrow")[i];
+    let left_arrow = document.getElementsByClassName("left-arrow")[i];
+    let carousel_items = document.getElementsByClassName("carousel-movies")[i];
+    let position = positions[i]
+
+    // Add EventListener to the right arrow.
+    right_arrow.addEventListener("click", function() {
+            position = Math.max(minPosition(), position - 1);
+            positions[i] = position
+            setArrowVisibility(position, left_arrow, right_arrow);
+            let translate = "translate(" + (1 / 7) * 100 * position + "%)"
+            carousel_items.style.transform = translate;
+            console.log("right-clicked - position = " + position + " / minPosition = " + minPosition());
+        })
+
+    // Add EventListener to the right arrow.
+    left_arrow.addEventListener("click", function() {
+            position = Math.min(0, position + 1);
+            positions[i] = position
+            setArrowVisibility(position, left_arrow, right_arrow);
+            let translate = "translate(" + (1 / 7) * 100 * position + "%)"
+            carousel_items.style.transform = translate;
+            console.log("left-clicked - position = " + position + " / Translate = " + translate);
+        })
+    setArrowVisibility(position, left_arrow, right_arrow)
+}
+
+// Define the minimum position of a given carousel
+function minPosition() {
+   let minPosition;
+    if (desktop.matches) {
+       minPosition = -3;
+    } else {
+       minPosition = -6;
+    }
+    return minPosition;
+}
+
+// Set the left and right arrows visibility.
+function setArrowVisibility(position, left_arrow, right_arrow) {
+    if (position === 0) {
+        left_arrow.style.visibility = "hidden"
+    } else {
+        left_arrow.style.visibility = "visible"
+    }
+    if (position === minPosition()) {
+        right_arrow.style.visibility = "hidden"
+    } else {
+        right_arrow.style.visibility = "visible"
+    }
+}
+
+
